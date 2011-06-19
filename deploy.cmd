5965fe3d9e645c263c284cdc029e79b90406d1e0
unclephil <koenigphil@gmail.com>
2011-06-19 14:58:46 +0200

    Signed-off-by: unclephil <koenigphil@gmail.com>

--- deploy.cmd
+++ deploy.cmd
@@ -1,8 +1,10 @@
 
 c:\python27\python.exe ./js2tiddler plugins/ServerSideSavingPlugin.js > fixtures/ServerSideSavingPlugin.json
 c:\python27\python.exe ./js2tiddler plugins/jquery-json.js > fixtures/jquery-json.json
+c:\python27\python.exe ./js2tiddler plugins/jquery-couch.js > fixtures/jquery-couch.json
 c:\python27\python.exe ./js2tiddler plugins/CouchdbAdaptor.js > fixtures/CouchDBAdaptor.json
 c:\python27\python.exe ./js2tiddler plugins/CouchdbConfig.js > fixtures/CouchDBConfig.json
 
 couchapp pushdocs fixtures %1
 couchapp push %1
+del fixtures/*
