b6c5be990bb6b84703f938cf5dabe19350580c72
unclephil <koenigphil@gmail.com>
2011-09-26 17:04:53 +0200

    modify validation syntax

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,18 @@
-mkdir ./fixtures
+mkdir .\fixtures
+
 
 c:\python27\python.exe ./js2tiddler plugins/ServerSideSavingPlugin.js > fixtures/ServerSideSavingPlugin.json
+
 c:\python27\python.exe ./js2tiddler plugins/jquery-json.js > fixtures/jquery-json.json
+
 c:\python27\python.exe ./js2tiddler plugins/jquery-couch.js > fixtures/jquery-couch.json
+
 c:\python27\python.exe ./js2tiddler plugins/CouchDBAdaptor.js > fixtures/CouchDBAdaptor.json
+
 c:\python27\python.exe ./js2tiddler plugins/CouchDBConfig.js > fixtures/CouchDBConfig.json
 
 couchapp pushdocs fixtures %1
+
 couchapp push %1
-del fixtures/*
+
+del fixtures\*
--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,21 +1,10 @@
 function(newDoc, oldDoc, userCtx) {
-
   function forbidden(message) {    
     throw({forbidden : message});
   };
-
-  function unauthorized(message) {
-    throw({unauthorized : message});
-  };
-
-  function require(beTrue, message) {
-    if (!beTrue) forbidden(message);
-  };
-
   // not logged in
   if (!userCtx.name) {
     forbidden("Sorry, you must be logged in to save this tiddler");
   }
-  
   return true;
 };
