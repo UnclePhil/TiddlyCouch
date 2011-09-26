ce3823b191ab4aa8723d8426a403a402b9e41e16
unclephil <koenigphil@gmail.com>
2011-09-26 14:27:10 +0200

    typo in deploy
    Signed-off-by: unclephil <koenigphil@gmail.com>

--- deploy.cmd
+++ deploy.cmd
@@ -4,7 +4,7 @@ c:\python27\python.exe ./js2tiddler plugins/ServerSideSavingPlugin.js > fixtures
 c:\python27\python.exe ./js2tiddler plugins/jquery-json.js > fixtures/jquery-json.json
 c:\python27\python.exe ./js2tiddler plugins/jquery-couch.js > fixtures/jquery-couch.json
 c:\python27\python.exe ./js2tiddler plugins/CouchDBAdaptor.js > fixtures/CouchDBAdaptor.json
-c:\python27\python.exe ./js2tiddler plugins/CouchABConfig.js > fixtures/CouchDBConfig.json
+c:\python27\python.exe ./js2tiddler plugins/CouchDBConfig.js > fixtures/CouchDBConfig.json
 
 couchapp pushdocs fixtures %1
 couchapp push %1
